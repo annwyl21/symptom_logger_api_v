9a6e466434c1589ed97070363c9386419792c6b9
Ellen Houghton <ellen.a.ash@gmail.com>
2023-11-02 18:02:52 +0000

    add date time formatting

---
+++ Visualize.py
@@ -0,0 +1,15 @@
+
+class Visualize():
+	
+	def scatterplot(dates, times):
+		# return url to the image
+		return "scatterplot"
+	
+	def bubbleplot(dates, times, pain):
+		# return url to the image
+		return "bubbleplot"
+
+
+if __name__ == '__main__':
+    Visualize.scatterplot()
+    Visualize.bubbleplot()
